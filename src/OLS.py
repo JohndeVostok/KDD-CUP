d8bad4b76808c94a6df8496c0ba3cc5ed1faa554
mazx <mazx15@mails.tsinghua.edu.cn>
2018-05-07 15:14:13 +0800

    test passed

--- src/OLS.py
+++ src/OLS.py
@@ -9,8 +9,6 @@ if __name__ == "__main__":
     l = len(dat[0])
     tpx = [];
     for i in range(l):
-        if i % 1000 == 0:
-            print(str(i)+'/'+str(l))
         tpx.append([])
         for j in range(651):
             tpx[i].append(dat[300 + j * 5][i])
@@ -18,7 +16,9 @@ if __name__ == "__main__":
             tpx[i].append(dat[300 + j * 5 + 2][i])
             tpx[i].append(dat[300 + j * 5 + 4][i])
     px = numpy.array(tpx).reshape(-1, 651 * 4)
-    print(px.shape)
+
+    print("grid data initialized.")    
+
     py = dat[0]
     linear = LinearRegression()
     linear.fit(px, py)
